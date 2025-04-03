ed28093b26f669bd81256e8877a2ac693d2fda70
darkmoongg <30297453@qq.com>
2025-04-03 10:16:33 +0800

    2025.4.3 daily quest, same as problem yesterday which id is 2873

---
+++ 2874MaximumValueofanOrderedTripletII.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        maxRight = [0] * n
+        maxRight[-1] = nums[-1]
+        for i in range(-2, -n -1, -1):
+            maxRight[i] = max(maxRight[i + 1], nums[i])
+        maxLeft = nums[0]
+        ans = 0
+        for i in range(1, n - 1):
+            t = (maxLeft - nums[i]) * maxRight[i + 1]
+            maxLeft = max(maxLeft, nums[i])
+            if t < 0:
+                continue
+            ans = max(ans, t)
+        return ans
